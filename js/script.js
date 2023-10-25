6616fcfc59712434b1f459c4e4686dbbc479ae41
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-10-25 10:46:31 +0200

    refactoring: condizioni eventlistner

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ console.log(thumbs);
 const slider = document.querySelector(".items")
 let sliderString = "";
 for (let i = 0; i < immaginiSlide.length; i++) {
-    console.log(immaginiSlide[i]);
     thumbsString += `<div class="thumb">
     <img src="${immaginiSlide[i]}" alt="">
 </div>`;
     sliderString += `<div class="item">
 <img src="${immaginiSlide[i]}" alt="">
-</div>`
+</div>`;
+
 }
 console.log(sliderString);
 console.log(thumbsString);
@@ -33,35 +33,28 @@ activeImmagine[activeImmagineIndex].classList.add("active");
 
 // Imposto le condizioni eventListner
 document.querySelector(".next").addEventListener("click", function () {
+
+    activeImmagine[activeImmagineIndex].classList.remove("active");
+    activeThumb[activeImmagineIndex].classList.remove("active");
+
     if (activeImmagineIndex < activeImmagine.length - 1) {
-        activeImmagine[activeImmagineIndex].classList.remove("active");
-        activeThumb[activeImmagineIndex].classList.remove("active");
         activeImmagineIndex++;
-        activeImmagine[activeImmagineIndex].classList.add("active")
-        activeThumb[activeImmagineIndex].classList.add("active");
-    } else if (activeImmagineIndex === activeImmagine.length - 1) {
-        activeThumb[activeImmagineIndex].classList.remove("active");
-        activeImmagine[activeImmagineIndex].classList.remove("active");
+        } else  {
         activeImmagineIndex = 0;
-        activeThumb[activeImmagineIndex].classList.add("active");
-        activeImmagine[activeImmagineIndex].classList.add("active")
     }
+    activeImmagine[activeImmagineIndex].classList.add("active")
+    activeThumb[activeImmagineIndex].classList.add("active");
 })
 
 document.querySelector(".prev").addEventListener("click", function () {
-    if (activeImmagineIndex > 0) {
-        activeImmagine[activeImmagineIndex].classList.remove("active");
+    activeImmagine[activeImmagineIndex].classList.remove("active");
         activeThumb[activeImmagineIndex].classList.remove("active");
-        activeImmagineIndex--;
-        activeThumb[activeImmagineIndex].classList.add("active");
-        activeImmagine[activeImmagineIndex].classList.add("active")
-    } else if (activeImmagineIndex === 0) {
-        activeThumb[activeImmagineIndex].classList.remove("active");
-        activeImmagine[activeImmagineIndex].classList.remove("active");
-        activeImmagineIndex = activeImmagine.length - 1;
-        activeThumb[activeImmagineIndex].classList.add("active");
-        activeImmagine[activeImmagineIndex].classList.add("active")
+    if (activeImmagineIndex > 0) { 
+        activeImmagineIndex--;     
+    } else {
+        activeImmagineIndex = activeImmagine.length - 1;  
     }
+    activeThumb[activeImmagineIndex].classList.add("active");
+    activeImmagine[activeImmagineIndex].classList.add("active");
 })
-
 
